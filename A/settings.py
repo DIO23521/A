a11ab8efcbbbfc82d2e89763a574a0fed8d0fe5c
DIO23521 <romashko.dima2003@gmail.com>
2026-04-09 17:37:31 +0200

    Added Login and Logout

--- A/settings.py
+++ A/settings.py
@@ -141,3 +141,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'  # Куда летим после логина (на главную)
+LOGOUT_REDIRECT_URL = '/' # Куда летим после выхода
